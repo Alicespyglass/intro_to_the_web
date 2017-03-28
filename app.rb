4e0d3d78d6fc8a3e15cc1f414c7c6ffbd6c21dde
Will Schwier <w.schwier@hotmail.co.uk>
2017-03-28 14:19:02 +0100

    Posted parameters

--- app.rb
+++ app.rb
@@ -9,16 +9,19 @@ get '/secret' do #localhost:4567/secret
   "We love HTML!"
 end
 
-get '/random-cat' do #localhost:4567/cat
+get '/random-cat' do #localhost:4567/random-cat
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
-get '/named-cat' do #localhost:4567/cat
+post '/named-cat' do #localhost:4567/named-cat
   p params
   @name = params[:name]
-
   erb(:index)
+end
+
+get '/name-form' do #localhost:4567/name-form
+  erb(:name_form)
 end
 
 
